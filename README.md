42a6db1a56dff781f017538828644a6bf27486d9
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2020-03-10 12:54:05 -0400

    init

---
+++ README.md
@@ -0,0 +1,68 @@
+# HashCharger Modal Widget
+
+Hashcharger can be integrated into any webpage as a Javscript widget and allows users to purchase hashing power from WariHash's spot market. 
+
+Demo link: https://devmarket.warihash.org/demo
+
+## Installation guide
+
+1. If you haven't done so already, register an account at https://market.warihash.com/register. Contact support@warihash.com and we'll give you a unique token that will be used to set up hashcharger with your account.
+
+1. Create a new button with 'open-hashcharger' class on your website. This button will be used to open HashCharger modal window. 
+
+```
+<button class="open-hashcharger">Example Button</button>
+```
+
+2. Add below script before closing body tag of your html file, change configurations as defined in the next section.
+
+```
+<script defer 
+  src="https://app.warihash.com/static/hashcharger.js"
+  id="hashcharger"
+  token="6j5KRGUkhUZfsXMgMATUgb"
+  stratum-host="stratum.slushpool.com" 
+  stratum-port="3333"
+  stratum-username="widgetaccount"
+  stratum-password="password"
+  algorithm="sha256d"
+  theme-navbg="3626a5"
+  theme-navtexts="ffffff"
+  theme-primary="3626a5"
+  theme-secondary="233f5c"
+  theme-buttontexts="ffffff"
+  theme-tabletexts="ffffff"
+  fullscreen="off">
+</script>
+```
+
+## Configurations
+
+#### id 
+don't change this ID parameter. id should be set to "hashcharger"
+
+#### token
+unique token value for authorization that we will give you. This identifies who you are to WariHash.
+
+#### stratum-host
+stratum host where hashing power will be directed to
+
+#### stratum-port
+port where hashing power will be directed to
+
+#### stratum-username
+stratum username that hashing power will be direted to
+
+#### stratum-password
+stratum password that hashing power wil be directed to
+
+#### algorithm
+algorithm to purchase hashing power for (currently available: sha256d, scrypt, handshake, ethash, equihash-zcash)
+
+#### theme-navbg
+#### theme-navtexts
+#### theme-primary
+#### theme-secondary
+#### theme-buttontexts
+#### theme-tabletexts
+#### fullscreen
---
+++ hashcharger.js
@@ -0,0 +1,79 @@
+
+var cssId = 'myCss';  
+if (!document.getElementById(cssId)) {
+   var head  = document.getElementsByTagName('head')[0];
+   var link  = document.createElement('link');
+   link.id   = cssId;
+   link.rel  = 'stylesheet';
+   link.type = 'text/css';
+   link.href = 'https://app.warihash.org/static/hashcharger.css';
+   link.media = 'all';
+   head.appendChild(link);
+};
+
+window.addEventListener("load", () => {
+
+var widgetOpenBtn = document.getElementsByClassName("open-hashcharger");
+
+var createModal = function() {
+    var root = document.getElementsByTagName( 'html' )[0];
+    root.classList.add("hashcharger-opened");
+    document.body.style.overflow = 'hidden';
+
+    var myScript = document.getElementById('hashcharger');
+    var token = (myScript.getAttribute('token') || '');
+    var host = (myScript.getAttribute('stratum-host') || '');
+    var port = (myScript.getAttribute('stratum-port') || '');
+    var username = (myScript.getAttribute('stratum-username') || '');
+    var password = (myScript.getAttribute('stratum-password') || '');
+    var algorithm = (myScript.getAttribute('algorithm') || '');
+    var navbg = (myScript.getAttribute('theme-navbg') || '');
+    var navtexts = (myScript.getAttribute('theme-navtexts') || '');
+    var primary = (myScript.getAttribute('theme-primary') || '');
+    var secondary = (myScript.getAttribute('theme-secondary') || '');
+    var buttontexts = (myScript.getAttribute('theme-buttontexts') || '');
+    var tabletexts = (myScript.getAttribute('theme-tabletexts') || '');
+    var fullscreen = (myScript.getAttribute('fullscreen') || '');
+
+    var modalWindow = document.createElement('div');
+    modalWindow.className = 'hashcharger-widget';
+    modalWindow.id = 'hashcharger-widget';
+    document.getElementsByTagName('body')[0].appendChild(modalWindow);
+
+
+    var iframe = document.createElement('iframe');
+    // eslint-disable-next-line no-useless-concat
+    iframe.src = 'https://app.warihash.com/market' + '/' + host + '/' + port + '/' + 
+    username + '/' + password + '/' + algorithm + '/' + navbg + '/' + navtexts + '/' + 
+    primary + '/' + secondary + '/' + buttontexts + '/' + tabletexts + '/' + token + '/' + fullscreen;
+    iframe.style.width = "100vw";
+    iframe.style.height = "100vh";
+    iframe.style.border = "none";
+    iframe.sandbox = 'allow-popups allow-popups-to-escape-sandbox allow-forms allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation';
+    modalWindow.appendChild(iframe);
+
+    var widgetCloseBtn = document.createElement('button');
+    widgetCloseBtn.type = 'button';
+    widgetCloseBtn.className = 'hashcharger-close-button';
+    widgetCloseBtn.id = 'hashcharger-close-button';
+    widgetCloseBtn.title = 'Close window';
+    widgetCloseBtn.innerHTML = '<svg class="svg-close-icon" enable-background="new 0 0 24 24" id="Layer_1" version="1.1" viewBox="0 0 24 24"><path d="M22.245,4.015c0.313,0.313,0.313,0.826,0,1.139l-6.276,6.27c-0.313,0.312-0.313,0.826,0,1.14l6.273,6.272  c0.313,0.313,0.313,0.826,0,1.14l-2.285,2.277c-0.314,0.312-0.828,0.312-1.142,0l-6.271-6.271c-0.313-0.313-0.828-0.313-1.141,0  l-6.276,6.267c-0.313,0.313-0.828,0.313-1.141,0l-2.282-2.28c-0.313-0.313-0.313-0.826,0-1.14l6.278-6.269  c0.313-0.312,0.313-0.826,0-1.14L1.709,5.147c-0.314-0.313-0.314-0.827,0-1.14l2.284-2.278C4.308,1.417,4.821,1.417,5.135,1.73  L11.405,8c0.314,0.314,0.828,0.314,1.141,0.001l6.276-6.267c0.312-0.312,0.826-0.312,1.141,0L22.245,4.015z"/></svg>';
+
+    widgetCloseBtn.addEventListener("click", function () {
+        var elem = document.getElementById('hashcharger-widget');
+        elem.classList.add('widget-fadeout');
+        setTimeout(function(){ 
+            elem.parentNode.removeChild(elem); 
+            var root = document.getElementsByTagName( 'html' )[0];
+            root.classList.remove("hashcharger-opened");
+            document.body.style.overflow = 'scroll';
+        }, 450);
+      });
+    modalWindow.appendChild(widgetCloseBtn);
+};
+
+for (var i = 0; i < widgetOpenBtn.length; i++) {
+    widgetOpenBtn[i].addEventListener('click', createModal, false);
+};
+
+});
